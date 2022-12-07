01fd730eba99f8f5acdca834d0a6012d79ff0a0f
jinhyeokfang <jinhyeokfang@gmail.com>
2022-12-07 14:27:41 +0000

    test: 품목이 없는 경우와 있는 경우 모두 검사

--- vending-machine/src/vending-machine.test.ts
+++ vending-machine/src/vending-machine.test.ts
@@ -2,8 +2,13 @@ import { VendingMachine } from "./vending-machine";
 
 describe('test VendingMachine', () => {
     it('a vending machine displays list of items', () => {
-        const vendingMachine = new VendingMachine();
-        const items = vendingMachine.getItems();
+        let vendingMachine = new VendingMachine([]);
+        let items = vendingMachine.getItems();
         expect(items).toEqual([]);
+
+        const LIST_OF_ITEMS = ['피자', '콜라', '오레오', '물티슈'];
+        vendingMachine = new VendingMachine(LIST_OF_ITEMS);
+        items = vendingMachine.getItems();
+        expect(items).toEqual(LIST_OF_ITEMS);
     });
 });
